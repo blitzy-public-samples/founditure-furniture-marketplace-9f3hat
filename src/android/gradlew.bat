6ccd335fef7246d1d34be78d7bcbc2f8286ce7cf
Siddhant P. Pardeshi <siddhantpp@gmail.com>
2024-11-19 15:45:41 -0500

    Add public-samples/founditure-furniture-marketplace-9f3hat/src/android/gradlew.bat

---
+++ src/android/gradlew.bat
@@ -0,0 +1,96 @@
+@rem
+@rem HUMAN TASKS:
+@rem 1. Ensure Java 1.8 or higher is installed and JAVA_HOME environment variable is set
+@rem 2. Verify network access to services.gradle.org for downloading Gradle distribution
+@rem 3. Ensure sufficient disk space for Gradle distribution (~100MB)
+@rem 4. Check write permissions in GRADLE_USER_HOME directory
+@rem
+
+@rem
+@rem Addresses requirements:
+@rem - Android Development Environment (4.1): Gradle build system for project compilation
+@rem - Development Tools (A.1): Gradle 8.0+ package manager
+@rem - Build System Configuration (6.1): Standardized build configuration
+@rem
+
+@if "%DEBUG%" == "" @echo off
+@rem ##########################################################################
+@rem
+@rem  Gradle startup script for Windows
+@rem
+@rem ##########################################################################
+
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" setlocal
+
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+
+@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS
+@rem to pass JVM options to this script.
+set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+
+@rem Find java.exe
+if defined JAVA_HOME goto findJavaFromJavaHome
+
+echo.
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+
+goto fail
+
+:findJavaFromJavaHome
+set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_EXE=%JAVA_HOME%/bin/java.exe
+
+if exist "%JAVA_EXE%" goto init
+
+echo.
+echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+
+goto fail
+
+:init
+@rem Get command-line arguments, handling Windows variants
+
+if not "%OS%" == "Windows_NT" goto win9xME_args
+
+:win9xME_args
+@rem Slurp the command line arguments.
+set CMD_LINE_ARGS=
+set _SKIP=2
+
+:win9xME_args_slurp
+if "x%~1" == "x" goto execute
+
+set CMD_LINE_ARGS=%*
+
+:execute
+@rem Setup the command line
+
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+
+@rem Execute Gradle
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
+
+:end
+@rem End local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" endlocal
+
+:omega
+if not "%ERRORLEVEL%"=="0" goto fail
+
+:fail
+exit /b 1
+
+:mainEnd
+if "%OS%"=="Windows_NT" endlocal
+
+exit /b %ERRORLEVEL%
